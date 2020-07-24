b422215d79348a3d2816013d902fd39b23d7d57b
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2020-07-24 18:48:30 +0300

    Update tonqq.py

--- tonqq.py
+++ tonqq.py
@@ -4,11 +4,11 @@ def _normal(_input: str) -> str:
     _encode = [int(_code) for _code in _character.encode('utf-8')]
     _colon = len(_encode) == 1
     _colon = _colon and _encode[0] == 58
-    _turk = len(_encode) == 4
-    _turk = _turk and _encode[0] == 240
-    _turk = _turk and _encode[1] == 144
-    _turk = _turk and (_encode[2] == 176 or _encode[2] == 177)
-    if _colon or _turk:
+    _tyrk = len(_encode) == 4
+    _tyrk = _tyrk and _encode[0] == 240
+    _tyrk = _tyrk and _encode[1] == 144
+    _tyrk = _tyrk and (_encode[2] == 176 or _encode[2] == 177)
+    if _colon or _tyrk:
       _value = _value + _character
     if _colon:
       _value = _value + '\u200f'
